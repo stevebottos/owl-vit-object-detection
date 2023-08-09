d2e6613ce72f8399157a335936cfb722baf9683a
stevebottos <bottos.steve@gmail.com>
2023-08-09 15:30:34 -0700

    WIP

--- src/losses.py
+++ src/losses.py
@@ -3,138 +3,13 @@ from scipy.optimize import linear_sum_assignment
 from torchvision.ops import box_area
 import numpy as np
 
-
-# modified from torchvision to also return the union
-def box_iou(boxes1, boxes2):
-    area1 = box_area(boxes1)
-    area2 = box_area(boxes2)
-
-    lt = torch.max(boxes1[:, None, :2], boxes2[:, :2])  # [N,M,2]
-    rb = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])  # [N,M,2]
-
-    wh = (rb - lt).clamp(min=0)  # [N,M,2]
-    inter = wh[:, :, 0] * wh[:, :, 1]  # [N,M]
-
-    union = area1[:, None] + area2 - inter
-
-    iou = inter / union
-    return iou, union
-
-
-# from https://github.com/facebookresearch/detr/blob/main/util/box_ops.py
-def generalized_box_iou(boxes1, boxes2):
-    """
-    Generalized IoU from https://giou.stanford.edu/
-    The boxes should be in [x0, y0, x1, y1] format
-    Returns a [N, M] pairwise matrix, where N = len(boxes1)
-    and M = len(boxes2)
-    """
-    # degenerate boxes gives inf / nan results
-    # so do an early check
-    assert (boxes1[:, 2:] >= boxes1[:, :2]).all()
-    assert (boxes2[:, 2:] >= boxes2[:, :2]).all()
-    iou, union = box_iou(boxes1, boxes2)
-
-    lt = torch.min(boxes1[:, None, :2], boxes2[:, :2])
-    rb = torch.max(boxes1[:, None, 2:], boxes2[:, 2:])
-
-    wh = (rb - lt).clamp(min=0)  # [N,M,2]
-    area = wh[:, :, 0] * wh[:, :, 1]
-
-    return iou - (area - union) / area
-
-
-# From https://github.com/facebookresearch/detr/blob/main/models/matcher.py
-class HungarianMatcher(torch.nn.Module):
-    """This class computes an assignment between the targets and the predictions of the network
-    For efficiency reasons, the targets don't include the no_object. Because of this, in general,
-    there are more predictions than targets. In this case, we do a 1-to-1 matching of the best predictions,
-    while the others are un-matched (and thus treated as non-objects).
-    """
-
-    def __init__(
-        self, cost_class: float = 1, cost_bbox: float = 1, cost_giou: float = 1
-    ):
-        """Creates the matcher
-        Params:
-            cost_class: This is the relative weight of the classification error in the matching cost
-            cost_bbox: This is the relative weight of the L1 error of the bounding box coordinates in the matching cost
-            cost_giou: This is the relative weight of the giou loss of the bounding box in the matching cost
-        """
-        super().__init__()
-        self.cost_class = cost_class
-        self.cost_bbox = cost_bbox
-        self.cost_giou = cost_giou
-        assert (
-            cost_class != 0 or cost_bbox != 0 or cost_giou != 0
-        ), "all costs cant be 0"
-
-    @torch.no_grad()
-    def forward(self, outputs, targets):
-        """Performs the matching
-        Params:
-            outputs: This is a dict that contains at least these entries:
-                 "pred_logits": Tensor of dim [batch_size, num_queries, num_classes] with the classification logits
-                 "pred_boxes": Tensor of dim [batch_size, num_queries, 4] with the predicted box coordinates
-            targets: This is a list of targets (len(targets) = batch_size), where each target is a dict containing:
-                 "labels": Tensor of dim [num_target_boxes] (where num_target_boxes is the number of ground-truth
-                           objects in the target) containing the class labels
-                 "boxes": Tensor of dim [num_target_boxes, 4] containing the target box coordinates
-        Returns:
-            A list of size batch_size, containing tuples of (index_i, index_j) where:
-                - index_i is the indices of the selected predictions (in order)
-                - index_j is the indices of the corresponding selected targets (in order)
-            For each batch element, it holds:
-                len(index_i) = len(index_j) = min(num_queries, num_target_boxes)
-        """
-        bs, num_queries = outputs["pred_logits"].shape[:2]
-
-        # We flatten to compute the cost matrices in a batch
-        out_prob = (
-            outputs["pred_logits"].flatten(0, 1).softmax(-1)
-        )  # [batch_size * num_queries, num_classes]
-        out_bbox = outputs["pred_boxes"].flatten(0, 1)  # [batch_size * num_queries, 4]
-
-        # Also concat the target labels and boxes
-        tgt_ids = torch.cat([v["labels"] for v in targets])
-        tgt_bbox = torch.cat([v["boxes"] for v in targets])
-
-        # Compute the classification cost. Contrary to the loss, we don't use the NLL,
-        # but approximate it in 1 - proba[target class].
-        # The 1 is a constant that doesn't change the matching, it can be ommitted.
-        cost_class = -out_prob[:, tgt_ids]
-
-        # Compute the L1 cost between boxes
-        cost_bbox = torch.cdist(out_bbox, tgt_bbox, p=1)
-
-        # Compute the giou cost betwen boxes
-        cost_giou = -generalized_box_iou(out_bbox, tgt_bbox)
-
-        # Final cost matrix
-        C = (
-            self.cost_bbox * cost_bbox
-            + self.cost_class * cost_class
-            + self.cost_giou * cost_giou
-        )
-        C = C.view(bs, num_queries, -1).cpu()
-
-        sizes = [len(v["boxes"]) for v in targets]
-        indices = [
-            linear_sum_assignment(c[i]) for i, c in enumerate(C.split(sizes, -1))
-        ]
-        return [
-            (
-                torch.as_tensor(i, dtype=torch.int64),
-                torch.as_tensor(j, dtype=torch.int64),
-            )
-            for i, j in indices
-        ]
+from src.matcher import HungarianMatcher, box_iou, generalized_box_iou
 
 
 class PushPullLoss(torch.nn.Module):
     def __init__(self, n_classes, scales):
         super().__init__()
-        self.matcher = HungarianMatcher()
+        self.matcher = HungarianMatcher(n_classes + 1)
         self.class_criterion = torch.nn.BCELoss(reduction="none", weight=scales)
 
         self.n_classes = n_classes
@@ -148,22 +23,12 @@ class PushPullLoss(torch.nn.Module):
         src_idx = torch.cat([src for (src, _) in indices])
         return batch_idx, src_idx
 
-    def class_loss(self, outputs, targets, indices):
+    def class_loss(self, outputs, target_classes):
         """
         Custom loss that works off of similarities
         """
+
         src_logits = outputs["pred_logits"]
-        idx = self._get_src_permutation_idx(indices)
-        target_classes_o = torch.cat(
-            [t["labels"][J] for t, (_, J) in zip(targets, indices)]
-        )
-        target_classes = torch.full(
-            src_logits.shape[:2],
-            self.null_class,
-            dtype=torch.int64,
-            device=src_logits.device,
-        )
-        target_classes[idx] = target_classes_o
         src_logits = src_logits.transpose(1, 2)
         assert target_classes.size(0) == 1  # TODO: batches
         target_classes.squeeze_(0)
@@ -184,13 +49,13 @@ class PushPullLoss(torch.nn.Module):
             dim=0
         ) / self.n_classes
 
-        neg_loss = (torch.pow(1 - torch.exp(-neg_loss), 2) * pos_loss).sum(
+        neg_loss = (torch.pow(1 - torch.exp(-neg_loss), 2) * neg_loss).sum(
             dim=0
         ) / self.n_classes
 
         return pos_loss.sum(), neg_loss.sum()
 
-    def loss_boxes(self, outputs, targets, indices, num_boxes):
+    def loss_boxes(self, outputs, targets, indices, idx, num_boxes):
         """
         (DETR box loss)
 
@@ -198,8 +63,8 @@ class PushPullLoss(torch.nn.Module):
         targets dicts must contain the key "boxes" containing a tensor of dim [nb_target_boxes, 4]
         The target boxes are expected in format (center_x, center_y, w, h), normalized by the image size.
         """
+
         assert "pred_boxes" in outputs
-        idx = self._get_src_permutation_idx(indices)
         src_boxes = outputs["pred_boxes"][idx]
         target_boxes = torch.cat(
             [t["boxes"][i] for t, (_, i) in zip(targets, indices)], dim=0
@@ -237,20 +102,31 @@ class PushPullLoss(torch.nn.Module):
             for _boxes, _labels in zip(target_boxes, target_classes)
         ]
 
-        indices = self.matcher(in_preds, in_targets)
+        target_classes, indices, idx = self.matcher(in_preds, in_targets)
         # Batch index is meaningless since we use a single batch
         # batch index is like [0,0,0,1,1,1,1,1,1,2,2,2,2,3,3] ... etc depending on number of batches
         # TODO: Generalize to more batches
         # target_classes.squeeze_(0)
         # predicted_classes.squeeze_(0)
 
-        loss_class, loss_background = self.class_loss(in_preds, in_targets, indices)
         loss_bbox, loss_giou = self.loss_boxes(
             in_preds,
             in_targets,
             indices,
+            idx,
             num_boxes=sum(len(t["labels"]) for t in in_targets),
         )
+
+        for box, label in zip(predicted_boxes[0], target_classes[0]):
+            if label == self.null_class:
+                continue
+
+            iou, _ = box_iou(box.unsqueeze(0), predicted_boxes.squeeze(0))
+            idx = iou > 0.8
+            target_classes[idx] = label.item()
+
+        loss_class, loss_background = self.class_loss(in_preds, target_classes)
+
         losses = {
             "loss_ce": loss_class,
             "loss_bg": loss_background,
